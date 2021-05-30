329d5f0775d4acb68ea9c7c906520c3108f703e6
RDNATH93 <im_rahuldebnath@outlook.com>
2021-05-30 11:58:50 +0530

    Add backend Match API

--- ipl-dashboard/src/main/java/com/iplt20/ipldashboard/controller/TeamController.java
+++ ipl-dashboard/src/main/java/com/iplt20/ipldashboard/controller/TeamController.java
@@ -3,8 +3,13 @@ package com.iplt20.ipldashboard.controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
+import java.util.List;
+
+import com.iplt20.ipldashboard.model.MatchSummary;
 import com.iplt20.ipldashboard.model.Team;
 import com.iplt20.ipldashboard.repository.MatchRepository;
 import com.iplt20.ipldashboard.repository.TeamRepository;
@@ -27,4 +32,11 @@ public class TeamController {
 		team.setMatchPlayed(matchRepository.listMatchesByTeamName(teamName, 4));
 		return team;
 	}
+	@GetMapping("/teams/{teamName}/matches")
+	public List<MatchSummary> getMatchesByTeamNameAndYear(@PathVariable String teamName,@RequestParam int year){
+		LocalDate startDate=LocalDate.of(year, 1, 1);
+		LocalDate endDate=LocalDate.of(year+1, 1, 1);
+		return matchRepository.getMatchesByTeamBetweenDates(teamName, startDate, endDate);
+	}
+
 }
--- ipl-dashboard/src/main/java/com/iplt20/ipldashboard/repository/MatchRepository.java
+++ ipl-dashboard/src/main/java/com/iplt20/ipldashboard/repository/MatchRepository.java
@@ -1,10 +1,13 @@
 package com.iplt20.ipldashboard.repository;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import com.iplt20.ipldashboard.model.MatchSummary;
 
@@ -12,7 +15,17 @@ public interface MatchRepository extends CrudRepository<MatchSummary,Long> {
 
 	List<MatchSummary> getByTeam1OrTeam2OrderByDateDesc(String teamName1,String teamName2,Pageable pageable);
 	
+	/*List<MatchSummary> getByTeam1AndDateBetweenOrTeam2AndDateBetweenOrderByDateDesc(
+		String teamName1,LocalDate startDate1,LocalDate endDate1,
+		String teamName2,LocalDate startDate2,LocalDate endDate2
+	);*/
+	@Query("select m from MatchSummary m where (m.team1=:teamName or m.team2=:teamName) and m.date between :startDate and :endDate order by m.date desc")
+	List<MatchSummary>getMatchesByTeamBetweenDates(@Param("teamName") String teamName,
+												 @Param("startDate")LocalDate startDate, @Param("endDate") LocalDate endDate);
+
 	default List<MatchSummary> listMatchesByTeamName(String teamName,int count){
 		return getByTeam1OrTeam2OrderByDateDesc(teamName, teamName, PageRequest.of(0, count));
 	}
+
+
 }
