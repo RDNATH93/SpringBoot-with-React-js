0431ae6d0f68ab2944857d8ec97004f42b29e9dd
RDNATH93 <im_rahuldebnath@outlook.com>
2021-05-30 09:36:37 +0530

    Added Router and Link

--- ipl-dashboard/src/frontend/src/components/MatchSmallCard.js
+++ ipl-dashboard/src/frontend/src/components/MatchSmallCard.js
@@ -1,9 +1,14 @@
 import { React } from 'react';
+import { Link } from 'react-router-dom';
 
-export const MatchSmallCard=({match})=> {
+export const MatchSmallCard=({teamName,match})=> {
+  const otherTeam= teamName===match.team1?match.team2 :match.team1;
+  const otherTeamRoute=`/teams/${otherTeam}`;
+
   return (
     <div className="MatchSmallCard">
-        <p>{match.team1} vs {match.team2}</p>
+        <h4>vs <Link to={otherTeamRoute}>{otherTeam}</Link></h4>
+        <p>{match.matchWinner} won by {match.resultMargin} {match.result}</p>
     </div>
   );
 }
