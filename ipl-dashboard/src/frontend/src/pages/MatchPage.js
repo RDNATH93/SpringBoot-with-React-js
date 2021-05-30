7e2bee6ef3ef7c2d6f048c2816d92c483936b99d
RDNATH93 <im_rahuldebnath@outlook.com>
2021-05-30 12:59:52 +0530

    Added Match API on MatchPage

--- ipl-dashboard/src/frontend/src/pages/MatchPage.js
+++ ipl-dashboard/src/frontend/src/pages/MatchPage.js
@@ -1,15 +1,32 @@
 import { React,useEffect,useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { MatchDetailsCard } from '../components/MatchDetailsCard';
-import { MatchSmallCard } from '../components/MatchSmallCard';
 
 export const MatchPage=()=> {
+ 
+  const [matches,setMatches]=useState([]);
+  const {teamName, year }=useParams();
 
+  useEffect(
+    ()=>{
+      const fetchMatches=async ()=>{
+          const response= await fetch(`http://localhost:8080/teams/${teamName}/matches?year=${year}`);
+          const data=await response.json();
+          console.log(data);
+          setMatches(data);
+      };
+      
+      fetchMatches();
+    },[teamName,year]
+  );
     
   return (
     <div className="MatchPage">
       <h1>Match Page</h1>
-      
+      {
+        matches.map(match =><MatchDetailsCard teamName={teamName} match={match} />)
+      }
     </div>
   );
 }
+ 
